7b7a0905bcefc5ae8ab9b9d4b6d377412854b469
vumiiliakonga2-ctrl <vumiiliakonga2@gmail.com>
2025-07-28 19:23:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,6 @@ def admin():
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # You can change the filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == '__main__':
     init_db()
