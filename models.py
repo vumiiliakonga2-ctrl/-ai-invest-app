fc97e3bee2b8da0fcb5ac784494f01c4574b675f
vumiiliakonga2-ctrl <vumiiliakonga2@gmail.com>
2025-07-28 18:38:22 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
